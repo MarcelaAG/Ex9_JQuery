e20a5d314241ccd657bc8e519a0b780b6c55c7a3
MarcelaAG <marcela.aguilerablanc@gmail.com>
2021-05-20 09:28:52 +0200

    give styles to an element

---
+++ main.js
@@ -0,0 +1,5 @@
+//The css() method sets or returns one or more style properties for the selected elements.
+$(document).ready(function(){
+    let changeItUp = $('#texte_1, #texte_3');
+    $(changeItUp).css("border","5px green dashed") 
+});    
